990a847b89e1732eb92d375ae813f36957b46e87
Muharrem Koç <80245013+muharremkoc@users.noreply.github.com>
2023-04-16 12:52:50 +0300

    Update UserServiceImpl.java

--- consumer-service/src/main/java/com/spring/consumer/service/UserServiceImpl.java
+++ consumer-service/src/main/java/com/spring/consumer/service/UserServiceImpl.java
@@ -33,6 +33,6 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userRepository.findAll();
     }
 }
